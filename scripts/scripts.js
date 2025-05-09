d36efa375e153569acdb950d4d2fb2670e65de32
Timothy Rutkowski <tim.rutkowski@gmail.com>
2025-05-09 01:00:13 +0000

    Update WEB215

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,5 @@
-
+    document.querySelectorAll('[data-include]').forEach((el) => {
+      fetch(el.getAttribute('data-include'))
+        .then((res) => res.text())
+        .then((html) => el.innerHTML = html);
+    });
